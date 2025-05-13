73a51274f7d3416443fb0f2d23c44823369ca711
Ciaran Hayes <hayes.c.hayes@gmail.com>
2025-05-13 23:59:20 +0200

    All genres added

--- QueerDirectoryAngular/src/app/genre-area/genre-area.component.ts
+++ QueerDirectoryAngular/src/app/genre-area/genre-area.component.ts
@@ -5,23 +5,47 @@ import { GetGenreService } from '../get-genre.service';
   selector: 'app-genre-area',
   imports: [],
   templateUrl: './genre-area.component.html',
-  styleUrl: './genre-area.component.css'
+  styleUrl: './genre-area.component.css',
 })
 export class GenreAreaComponent implements OnInit {
   books: any[] = [];
-  genre: string = 'Romance'
+  genres: string[] = [
+    'Romance',
+    'Coming of Age',
+    'Literary Fiction',
+    'Fantasy',
+    'Young Adult',
+    'Contemporary',
+    'Adventure',
+    'Lesbian Fiction',
+    'LGBTQIA+',
+    'Historical Fiction',
+    'Graphic Novel',
+    'Memoir',
+    'Transgender',
+    'Non-fiction',
+    'Gothic Fiction',
+    'Philosophy',
+    'Thriller',
+    'Mystery',
+    'Comedy',
+    'Poetry',
+    'Science Fiction',
+    'Horror',
+  ];
 
   constructor(private getGenreService: GetGenreService) {}
 
   ngOnInit(): void {
-    this.getGenreService.getGenre(this.genre).subscribe({
-      next: (data) => {
-        this.books = data;
-      },
-      error: (error) => {
-        console.error('Failed to fetch books', error);
-      }
-    });
+    for (let i=0; i < this.genres.length; i++) {
+      this.getGenreService.getGenre(this.genres[i]).subscribe({
+        next: (data) => {
+          this.books = data;
+        },
+        error: (error) => {
+          console.error('Failed to fetch books', error);
+        },
+      });
+    }
   }
-
 }
